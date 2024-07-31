6eb5fc399462494371e0f234e6928026dd920e15
Alok Yadav <yalok6321@gmail.com>
2024-07-31 10:32:44 +0530

    config of some express middleware and of cors and cookie-parser

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 3000;
 
 connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port : ${PORT}`);
+        })
+    })
+    .catch( (error) => {
+        console.error("Error connecting to the database: ", error);
+        process.exit(1);
+    })
