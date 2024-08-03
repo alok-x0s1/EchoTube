031166f0a1ba3d7e14b365568d2ab3047e4f5716
Alok Yadav <yalok6321@gmail.com>
2024-08-03 12:21:00 +0530

    multer middleware

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
         cb(null, "./public/temp");
     },
     filename: (req, file, cb) => {
-        cb(null, new Date().toISOString() + "_" + file.originalname);
+        cb(null, file.originalname);
     },
 });
 
