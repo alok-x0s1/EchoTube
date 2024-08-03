f636bcea94f1089ca852ff031c84525fb0f2b9fa
Alok Yadav <yalok6321@gmail.com>
2024-08-03 12:20:52 +0530

    Cloudinary config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -4,7 +4,7 @@ import fs from "fs";
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_API_SECREAT,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -13,10 +13,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto",
         });
-        console.log(
-            "File has been uploaded successfully to cloudinary.",
-            response
-        );
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         console.error("Error uploading file to cloudinary:", error);
