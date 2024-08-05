4dce115f28248afec11d2ccc72c28bfca0a0dcbe
Alok Yadav <yalok6321@gmail.com>
2024-08-05 11:00:26 +0530

    added user controller to routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import {
+    changeCurrentPassword,
+    getCurrentUser,
     loginUser,
     logoutUser,
+    refreshAccessToken,
     registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -22,7 +28,17 @@ router.route("/register").post(
     registerUser
 );
 
+router.route("/me").get(verifyJWT, getCurrentUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("update-password").put(verifyJWT, changeCurrentPassword);
+router.route("/update-account").put(verifyJWT, updateAccountDetails);
+router
+    .route("/update-avatar")
+    .put(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("update-cover-image")
+    .put(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 export default router;
