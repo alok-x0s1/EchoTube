d446dddb286163d49fbdae4957330323a3874b2b
Alok Yadav <yalok6321@gmail.com>
2024-08-05 10:54:25 +0530

    Subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+    {
+        subscriber: {
+            type: mongoose.Schema.Types.ObjectId, // One who is subscribing
+            ref: "User",
+        },
+        channel: {
+            type: mongoose.Schema.Types.ObjectId, // One to whome 'subscriber' is subscribing
+            ref: "User",
+        },
+    },
+    { timestamps: true }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
