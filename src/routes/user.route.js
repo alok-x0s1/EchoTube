d7f85c38f57695a448cb28c72379f850f494a2f0
Alok Yadav <yalok6321@gmail.com>
2024-08-07 10:27:04 +0530

    Added all contoller in routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
     changeCurrentPassword,
     getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
     loginUser,
     logoutUser,
     refreshAccessToken,
@@ -40,5 +42,7 @@ router
 router
     .route("/update-coverImage")
     .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
