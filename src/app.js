c32d6ad9520158dadd8e1c490186f4a76fde3331
Alok Yadav <yalok6321@gmail.com>
2024-08-09 10:30:15 +0530

    Config routes in app.js

--- src/app.js
+++ src/app.js
@@ -20,10 +20,12 @@ app.use(cookieParser());
 // Import routes
 import userRouter from "./routes/user.route.js"
 import videoRouter from "./routes/video.route.js"
+import tweetRouter from "./routes/tweet.route.js"
 
 // Routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 
 // Error handling middleware
