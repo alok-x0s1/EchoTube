0a7c419bdfeae4b5e44b80a00163641255002c75
Alok Yadav <yalok6321@gmail.com>
2024-08-12 09:10:17 +0530

    configure subscription routes for api

--- src/app.js
+++ src/app.js
@@ -21,11 +21,13 @@ app.use(cookieParser());
 import userRouter from "./routes/user.route.js"
 import videoRouter from "./routes/video.route.js"
 import tweetRouter from "./routes/tweet.route.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 // Routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 
 // Error handling middleware
