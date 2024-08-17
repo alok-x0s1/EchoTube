52d696d9fa9672b437039d8611ae26a8d0b8b81e
Alok Yadav <yalok6321@gmail.com>
2024-08-17 10:18:52 +0530

    Dashboard route

---
+++ src/routes/dashboard.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { getChannelStats, getChannelVideos } from "../controllers/dashboard.controller.js";
+
+const router = express.Router();
+router.use(verifyJWT);
+
+router.route("/stats").get(getChannelStats);
+router.route("/videos").get(getChannelVideos);
+
+export default router;
